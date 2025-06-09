56378c4f355e463e5e302c6586910e1461c90c64
soumyajyoti mohanta <soumyajyotimohanta@gmail.com>
2025-06-09 22:14:13 +0530

    fix: Mindlog page calandar button not clicking because of styled mindlog text z index covering the calandar button

--- Cassidy-Talking-Avatar/src/app/dashboard/mindlog/page.tsx
+++ Cassidy-Talking-Avatar/src/app/dashboard/mindlog/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <HeroHeading></HeroHeading>
         <HeroSubheading></HeroSubheading>
       </div>
-      <div className="banner-text fixed z-[0] w-[100vh] h-auto top-1/2 transform -translate-y-1/2 right-0 text-[120px] font-[900] text-[#ffffff] text-center opacity-15 origin-center -rotate-90 translate-x-[calc(50%-0.5em)] select-none">
+      <div className="banner-text fixed z-[-1] w-[100vh] h-auto top-1/2 transform -translate-y-1/2 right-0 text-[120px] font-[900] text-[#ffffff] text-center opacity-15 origin-center -rotate-90 translate-x-[calc(50%-0.5em)] select-none">
             MINDLOG
           </div>
       <JournalForm />
